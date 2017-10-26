c7ad2b2f978351389db6857c6fb2c0a083173741
cesarchretien <cesarchre10@gmail.com>
2017-10-26 23:20:24 +0200

    Initial commit

---
+++ app/src/main/java/com/devinitive/cesarchretien/associateandroiddevelopertraining/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devinitive.cesarchretien.associateandroiddevelopertraining
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
